4993ae541530c3ab4219b4ac471ae10045e47e48
ginius4ginius <derieu.vincent@outlook.com>
2018-02-21 06:32:00 +0100

    ce matin

--- ejbModule/model/Auteur.java
+++ ejbModule/model/Auteur.java
@@ -47,7 +47,6 @@ public class Auteur implements Serializable {
 
 	public short getNum() {
 		return this.num;
-		//System.out.println("mon nom est vincent et je suis le meilleur");
 	}
 
 	public void setNum(short num) {
